48fcbe3e7cbd53e07c94dc6ba6651930f53b25a9
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-06 03:21:38 +0600

    create delete api for manage my food

--- index.js
+++ index.js
@@ -70,6 +70,13 @@ async function run() {
       }
     });
 
+    app.delete("/foods/:id",async (req, res)=> {
+      const id = req.params
+      const cursor = { _id : new ObjectId(id)}
+      const result = await foodCollection.deleteOne(cursor)
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
